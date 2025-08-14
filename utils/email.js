355fa0a3ccc2e07b023213a41deda69b30eac77f
dazer-m-l <dazer.mc1397@gmail.com>
2025-08-13 19:20:59 -0600

    Gestion de pruebas para credenciales Gmail

--- utils/email.js
+++ utils/email.js
@@ -1,14 +1,21 @@
 const nodemailer = require('nodemailer');
 require('dotenv').config();
 
+console.log('=== DEBUG EMAIL CONFIG ===');
+console.log('EMAIL_ADDRESS:', process.env.EMAIL_ADDRESS);
+console.log('EMAIL_PASSWORD length:', process.env.EMAIL_PASSWORD ? process.env.EMAIL_PASSWORD.length : 'undefined');
+console.log('Todas las env vars:', Object.keys(process.env).filter(key => key.includes('EMAIL')));
+console.log('===========================');
+
 const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    host: 'smtp.gmail.com',
+    port: 587,
+    secure: false,
     auth: {
         user: process.env.EMAIL_ADDRESS,
         pass: process.env.EMAIL_PASSWORD
     }
 });
-
 const sendEmail = async (email, subject, text) => {
     try {
         await transporter.sendMail({
